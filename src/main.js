8926cc4c30f97c368ef0d4d0d01c7343511db42f
Ben Lenser <lenser.ben@gmail.com>
2026-01-18 21:48:03 +0100

    feat: initialize Phaser 3 project with basic config

--- src/main.js
+++ src/main.js
@@ -1,4 +1,12 @@
+import Phaser from 'phaser';
 
-const config = {};
+const config = {
+    type: Phaser.AUTO,
+    width: 1920,
+    height: 1080,
+    backgroundColor: '#2d2d2d',
+    parent: 'game-container',
+    scene: []
+};
 
 const game = new Phaser.Game(config);
