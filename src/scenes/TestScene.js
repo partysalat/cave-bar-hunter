cbac59ca2643a78d61e5ee26df048fe2f1caf716
Ben Lenser <lenser.ben@gmail.com>
2026-01-18 22:11:48 +0100

    feat: wire player movement to WASD keyboard input

--- src/scenes/TestScene.js
+++ src/scenes/TestScene.js
@@ -1,6 +1,7 @@
 import Phaser from 'phaser';
 import Player from '../entities/Player.js';
 import { worldToScreen } from '../systems/CoordinateSystem.js';
+import InputManager from '../systems/InputManager.js';
 
 /**
  * Test scene for Phase 1 development
@@ -18,6 +19,10 @@ export default class TestScene extends Phaser.Scene {
         // Create test player at arena center
         this.player = new Player(this, 0, 15, 12, 0);
 
+        // Setup input
+        this.inputManager = new InputManager(this);
+        this.inputManager.setupKeyboard(); // For testing without gamepad
+
         // Add debug text
         this.debugText = this.add.text(10, 10, '', {
             font: '16px monospace',
@@ -28,13 +33,27 @@ export default class TestScene extends Phaser.Scene {
 
     update(time, delta) {
         if (this.player) {
+            // Get input for player 0
+            const input = this.inputManager.getPlayerInputWithKeyboard(0);
+
+            if (input) {
+                const direction = this.inputManager.getDPadDirection(input.dpad);
+
+                if (direction.x !== 0 || direction.y !== 0) {
+                    this.player.move(direction.x, direction.y);
+                } else {
+                    this.player.stop();
+                }
+            }
+
             this.player.update(delta);
 
             // Update debug info
             this.debugText.setText([
                 `World: (${this.player.worldX.toFixed(1)}, ${this.player.worldY.toFixed(1)}, ${this.player.worldZ.toFixed(1)})`,
                 `Screen: (${this.player.sprite.x.toFixed(0)}, ${this.player.sprite.y.toFixed(0)})`,
-                `Depth: ${this.player.getDepth()}`
+                `Velocity: (${this.player.velocityX.toFixed(1)}, ${this.player.velocityY.toFixed(1)})`,
+                `Controls: WASD to move`
             ]);
         }
     }
